de6054c3c7e07fdc6fbcc360a66785dd863b8987
unknown <rokas.rakauskas10@gmail.com>
2021-03-23 17:59:42 +0200

    trecias

---
+++ app/Http/Controllers/CharacterController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Character;
+
+class CharacterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Character::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Character::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Character::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $character = Character::findOrFail($id);
+        $character->update($request->all());
+
+        return $character;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $character = Character::findOrFail($id);
+        $character->delete();
+
+        return 204;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GameController;
+use App\Http\Controllers\CharacterController;
 
 Route::get('/games',[GameController::class, 'index']);
 Route::get('/games/{id}',[GameController::class, 'show']);
 Route::post('/games',[GameController::class, 'store']);
 Route::put('/games/{id}',[GameController::class, 'update']);
 Route::delete('/games/{id}',[GameController::class, 'delete']);
+
+Route::get('/characters',[CharacterController::class, 'index']);
+Route::get('/characters/{id}',[CharacterController::class, 'show']);
+Route::post('/characters',[CharacterController::class, 'store']);
+Route::put('/characters/{id}',[CharacterController::class, 'update']);
+Route::delete('/characters/{id}',[CharacterController::class, 'delete']);
 
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
